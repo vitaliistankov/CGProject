43f54b75dd32be62965c580cbf632423b87b7f4d
Vitalii <36083562+vitaliistankov@users.noreply.github.com>
2019-05-10 19:37:47 +0300

    Adding geometric.processor

---
+++ src/Processors/GeometricProcessor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Draw
+{
+    /// <summary>
+    /// Класът, който ще бъде използван при управляване на диалога.
+    /// </summary>
+    public class GeometricProcessor : DisplayProcessor
+    {
+        #region Constructor
+
+        public GeometricProcessor()
+        {
+        }
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        public override void DrawShape(Graphics grfx, Shape item)
+        {
+
+            grfx.Transform = new Matrix();
+            grfx.Transform.RotateAt(20, new PointF(item.Location.X + item.Width / 2, item.Location.Y + item.Height/2));
+
+            base.DrawShape(grfx, item);
+                        
+            //item.DrawSelf(grfx);
+        }
+
+    }
+}
